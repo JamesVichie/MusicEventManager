db4f1aa400f19cff8f48d425c348479469d48ec3
James Vichie <plecherj@icloud.com>
2025-02-04 01:04:59 +1100

    First Commit!

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.idk.demo {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.idk.demo to javafx.fxml;
+    exports com.idk.demo;
+}
